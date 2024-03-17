2007a87fdb1ba9166037c2216de0933d4d94c7f0
Etienne Robert <etiennerobert33@gmail.com>
2024-03-17 11:08:03 +0100

    Use shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="text-white h-screen grid place-items-center">
+    <main className="dark text-white h-screen grid place-items-center">
       <Image
         // className="absolute right-0 top-0 h-screen w-auto"
         className="absolute object-cover h-full z-[-1]"
@@ -13,7 +14,7 @@ export default function Home() {
       />
       <div>
         <h1 className="text-5xl">La Diablerie</h1>
-        <button>Book Now</button>
+        <Button>Book Now</Button>
       </div>
     </main>
   );
