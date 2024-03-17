a7ab45803ee23fb53e6021367111cbb0ae99d6b4
Etienne Robert <etiennerobert33@gmail.com>
2024-03-17 12:47:18 +0100

    WIP

---
+++ src/components/MeetTheTeam.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const MeetTheTeam = () => (
+  <section className="grid p-4 gap-4">
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Meet the Team
+    </h1>
+    <ul className="grid gap-4">
+      <li>
+        <Image
+          className="max-w-[30rem]"
+          src="/ratboi_ttt.jpeg"
+          width={1280}
+          height={1600}
+          alt="Portrait of Ratboi"
+        />
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Ratboi
+        </h2>
+      </li>
+      <li>
+        <Image
+          className="max-w-[30rem]"
+          src="/cagoule_girl.jpeg"
+          width={1440}
+          height={1800}
+          alt="Portrait of Cagoule Girl"
+        />
+        <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+          Cagoule Girl
+        </h2>
+      </li>
+    </ul>
+  </section>
+);
+export default MeetTheTeam;
