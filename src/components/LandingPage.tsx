41d53ad87ddacc17352f8ded20f5111f18ef4ec3
Etienne Robert <etiennerobert33@gmail.com>
2024-03-30 18:51:30 +0100

    Add a fade on the bottom of the landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,7 +5,7 @@ import LearnMoreButton from "./LearnMoreButton";
 import studioImage from "../images/studio-4.jpeg";
 
 const LandingPage = () => (
-  <section className="grid h-full place-content-center justify-items-center gap-4">
+  <section className="grid h-full place-content-center justify-items-center gap-4 after:absolute after:bottom-0 after:left-0 after:right-0 after:top-[80%] after:bg-gradient-to-b after:from-transparent after:to-black">
     <Image
       className="absolute z-[-1] h-full object-cover brightness-50"
       src={studioImage}
