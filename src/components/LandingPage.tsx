35364cdba042e11ce0a76854fdc2b499180da335
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 12:27:57 +0200

    Move text back in center of screen

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -8,24 +8,26 @@ const gradient =
   "after:absolute after:bottom-0 after:left-0 after:right-0 after:top-[80%] after:bg-gradient-to-b after:from-transparent after:to-black";
 
 const LandingPage = () => (
-  <section className="relative grid h-[120%] place-content-center justify-items-center gap-4">
+  <section className="relative h-[120%]">
     <Image
       className="absolute z-[-1] h-full object-cover brightness-50"
       src={studioImage}
       placeholder="blur"
       alt="Picture of the studio"
     />
-    <h1 className="scroll-m-20 font-hot-winner-team text-6xl text-primary lg:text-8xl">
-      LA DIABLERIE
-    </h1>
-    <div className="flex gap-2">
-      <Link
-        href="https://www.instagram.com/ladiablerietattoo"
-        className={buttonVariants()}
-      >
-        Book Now
-      </Link>
-      <LearnMoreButton />
+    <div className="grid h-screen place-content-center">
+      <h1 className="scroll-m-20 font-hot-winner-team text-6xl text-primary lg:text-8xl">
+        LA DIABLERIE
+      </h1>
+      <div className="flex justify-center gap-2">
+        <Link
+          href="https://www.instagram.com/ladiablerietattoo"
+          className={buttonVariants()}
+        >
+          Book Now
+        </Link>
+        <LearnMoreButton />
+      </div>
     </div>
   </section>
 );
