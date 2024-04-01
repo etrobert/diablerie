c148bbf830f7dc7215a4998c3ea8a6523f5d4508
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 12:52:15 +0200

    Add dialog demo

--- src/components/MeetTheTeamTattooer.tsx
+++ src/components/MeetTheTeamTattooer.tsx
@@ -1,9 +1,22 @@
-import { Tattooer } from "@/data/getTeam";
 import { CSSProperties } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+import { Tattooer } from "@/data/getTeam";
 
 type Props = {
   tattooer: Tattooer;
 };
+
+const DialogDemo = () => (
+  <Dialog>
+    <DialogTrigger asChild>
+      <Button variant="outline">Tattoos</Button>
+    </DialogTrigger>
+    <DialogContent>Photos of Tattoos</DialogContent>
+  </Dialog>
+);
 
 const MeetTheTeamTattooer = ({ tattooer }: Props) => {
   const { name } = tattooer.fields;
@@ -14,7 +27,9 @@ const MeetTheTeamTattooer = ({ tattooer }: Props) => {
       <div
         className="h-full w-full bg-[image:var(--url)] bg-cover bg-center brightness-75"
         style={{ "--url": `url(https:${coverPicture.url})` } as CSSProperties}
-      />
+      >
+        <DialogDemo />
+      </div>
       <h2 className="absolute bottom-0 px-2 text-2xl font-semibold tracking-tight lg:px-4 lg:pb-2 lg:text-4xl">
         {name}
       </h2>
