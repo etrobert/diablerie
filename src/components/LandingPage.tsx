789aff9fc8d52b65252e9bdbaab94a95aeb13ba6
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 12:04:33 +0200

    Extract gradient attributes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,8 +4,11 @@ import { buttonVariants } from "@/components/ui/button";
 import LearnMoreButton from "./LearnMoreButton";
 import studioImage from "../images/studio-4.jpeg";
 
+const gradient =
+  "after:absolute after:bottom-0 after:left-0 after:right-0 after:top-[80%] after:bg-gradient-to-b after:from-transparent after:to-black";
+
 const LandingPage = () => (
-  <section className="grid h-full place-content-center justify-items-center gap-4 after:absolute after:bottom-0 after:left-0 after:right-0 after:top-[80%] after:bg-gradient-to-b after:from-transparent after:to-black">
+  <section className="grid h-full place-content-center justify-items-center gap-4">
     <Image
       className="absolute z-[-1] h-full object-cover brightness-50"
       src={studioImage}
