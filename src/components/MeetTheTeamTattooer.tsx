5333a5b5fe7c4d47c3c8cffcf664cfd24b963e66
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 14:41:18 +0200

    Add Maximise icon

--- src/components/MeetTheTeamTattooer.tsx
+++ src/components/MeetTheTeamTattooer.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 
 import Image from "next/image";
+import { Maximize2 } from "lucide-react";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import {
@@ -32,6 +33,10 @@ const MeetTheTeamTattooer = ({ tattooer }: Props) => {
               { "--url": `url(https:${coverPicture.url})` } as CSSProperties
             }
           >
+            <Maximize2
+              size={64}
+              className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+            />
             <h2 className="absolute bottom-0 px-2 text-2xl font-semibold tracking-tight lg:px-4 lg:pb-2 lg:text-4xl">
               {name}
             </h2>
