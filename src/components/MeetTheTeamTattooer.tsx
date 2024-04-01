2138d43e1300efab8191428d07a9edabd26bb373
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 15:20:14 +0200

    Extract TattoosCarousel component

--- src/components/MeetTheTeamTattooer.tsx
+++ src/components/MeetTheTeamTattooer.tsx
@@ -1,18 +1,11 @@
-import { CSSProperties } from "react";
-
-import Image from "next/image";
+import type { CSSProperties } from "react";
 import { Maximize2 } from "lucide-react";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
 
-import { Tattooer } from "@/data/getTeam";
+import TattoosCarousel from "./TattoosCarousel";
+
+import type { Tattooer } from "@/data/getTeam";
 
 type Props = {
   tattooer: Tattooer;
@@ -44,28 +37,7 @@ const MeetTheTeamTattooer = ({ tattooer }: Props) => {
           </div>
         </DialogTrigger>
         <DialogContent>
-          <Carousel>
-            <CarouselContent>
-              {tattoos?.map((tattoo) => {
-                const { file, description } = tattoo.fields;
-                const { width, height } = file.details.image;
-                const url = "https:" + file.url;
-                return (
-                  <CarouselItem key={url}>
-                    <Image
-                      key={url}
-                      src={url}
-                      width={width}
-                      height={height}
-                      alt={description}
-                    />
-                  </CarouselItem>
-                );
-              })}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
+          {tattoos && <TattoosCarousel tattoos={tattoos} />}
         </DialogContent>
       </Dialog>
     </li>
