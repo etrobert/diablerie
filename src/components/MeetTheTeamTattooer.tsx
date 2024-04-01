84e7919e67570833f61aa2d6a185143637992e0c
Etienne Robert <etiennerobert33@gmail.com>
2024-04-01 15:30:34 +0200

    Make tattoos rendering conditional

--- src/components/MeetTheTeamTattooer.tsx
+++ src/components/MeetTheTeamTattooer.tsx
@@ -1,10 +1,11 @@
-import type { CSSProperties } from "react";
 import { Maximize2 } from "lucide-react";
 
+import { cn } from "@/lib/utils";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 import TattoosCarousel from "./TattoosCarousel";
 
+import type { CSSProperties } from "react";
 import type { Tattooer } from "@/data/getTeam";
 
 type Props = {
@@ -16,30 +17,39 @@ const MeetTheTeamTattooer = ({ tattooer }: Props) => {
   const coverPicture = tattooer.fields.coverPicture.fields.file;
   const tattoos = tattooer.fields.tattoos;
 
+  const column = (
+    <div className={cn("relative h-full", tattoos && "group cursor-pointer")}>
+      <div
+        className={cn(
+          "h-full w-full bg-[image:var(--url)] bg-cover bg-center brightness-75 ",
+          tattoos && "transition-[filter] group-hover:brightness-50",
+        )}
+        style={{ "--url": `url(https:${coverPicture.url})` } as CSSProperties}
+      />
+      {tattoos && (
+        <Maximize2
+          size={64}
+          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity group-hover:opacity-100"
+        />
+      )}
+      <h2 className="absolute bottom-0 px-2 text-2xl font-semibold tracking-tight lg:px-4 lg:pb-2 lg:text-4xl">
+        {name}
+      </h2>
+    </div>
+  );
+
   return (
     <li key={name}>
-      <Dialog>
-        <DialogTrigger asChild>
-          <div className="group relative h-full cursor-pointer">
-            <div
-              className="h-full w-full bg-[image:var(--url)] bg-cover bg-center brightness-75 transition-[filter] group-hover:brightness-50"
-              style={
-                { "--url": `url(https:${coverPicture.url})` } as CSSProperties
-              }
-            />
-            <Maximize2
-              size={64}
-              className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity group-hover:opacity-100"
-            />
-            <h2 className="absolute bottom-0 px-2 text-2xl font-semibold tracking-tight lg:px-4 lg:pb-2 lg:text-4xl">
-              {name}
-            </h2>
-          </div>
-        </DialogTrigger>
-        <DialogContent>
-          {tattoos && <TattoosCarousel tattoos={tattoos} />}
-        </DialogContent>
-      </Dialog>
+      {tattoos ? (
+        <Dialog>
+          <DialogTrigger asChild>{column}</DialogTrigger>
+          <DialogContent>
+            <TattoosCarousel tattoos={tattoos} />
+          </DialogContent>
+        </Dialog>
+      ) : (
+        column
+      )}
     </li>
   );
 };
